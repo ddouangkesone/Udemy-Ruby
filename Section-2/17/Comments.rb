3c82c358a2afa6c5fcdcb53a4d0ca84e4a54da39
Daniel Douangkesone <karateporkchop@gmail.com>
2018-12-14 15:12:24 -0600

    finished lesson 17

---
+++ Section-2/17/Comments.rb
@@ -0,0 +1,13 @@
+# This is a comment
+
+puts "Hello World" # Prints "Hello world" to the console
+
+# In this section of code,
+# we are going to do operations
+# A, B, and C
+
+=begin
+
+blah blah blah
+
+=end
