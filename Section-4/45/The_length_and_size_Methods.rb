fae106f5a7a3d2744744b5b7cb54c023827657f9
Daniel Douangkesone <karateporkchop@gmail.com>
2018-12-18 17:06:13 -0600

    finished

---
+++ Section-4/45/The_length_and_size_Methods.rb
@@ -0,0 +1,18 @@
+a = "Hello World"
+b = "Hi, my name is Dan!"
+c = " "
+d = ""
+
+# p a.length
+# p b.length
+# p c.length
+# p d.length
+
+# puts
+
+# p a.size
+# p b.size
+# p c.size
+# p d.size
+
+a.length.odd?
