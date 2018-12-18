94aa681a856b3d4a7290bc3a10e09e5f10d1da37
Daniel Douangkesone <karateporkchop@gmail.com>
2018-12-18 16:39:55 -0600

    finished

---
+++ Section-4/41/Escape_Characters.rb
@@ -0,0 +1,15 @@
+# puts "Hello world"
+# puts 'Hello world'
+
+# puts "Juliet said 'Goodbye' to Romeo"
+# puts 'Juliet said "Goodbye" to Romeo'
+
+# puts "Juliet said \"Goodbye\" to Romeo"
+# puts 'Juliet said \'Goodbye\' to Romoe'
+
+# result =  "Let's add a line break\nright here"
+
+# puts result
+# p result
+
+puts "\tInserts a tab right here"
