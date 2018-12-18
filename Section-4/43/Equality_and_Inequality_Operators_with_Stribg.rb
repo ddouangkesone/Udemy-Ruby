33277259c7c58cd082fc43775375a0dbc72ba1ff
Daniel Douangkesone <karateporkchop@gmail.com>
2018-12-18 16:49:09 -0600

    finished

---
+++ Section-4/43/Equality_and_Inequality_Operators_with_Stribg.rb
@@ -0,0 +1,19 @@
+# a = "Hello"
+# b = "hello"
+# c = "Hello"
+
+# p a == b
+# p a == c
+# p b == c
+# p a == a
+
+# p a != b
+# p a != c
+
+# p "Apple" < "Banana"
+# p "hello" < "help"
+
+p "A" < "a"
+p "Z" < "a"
+
+p "Help" < "Banana"
