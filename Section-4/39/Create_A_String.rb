ad08728870ecf3cff30c2d2f439065888241da86
Daniel Douangkesone <karateporkchop@gmail.com>
2018-12-18 16:17:02 -0600

    finished

---
+++ Section-4/39/Create_A_String.rb
@@ -0,0 +1,26 @@
+# puts "Hi there I'm a string"
+# puts "I can also include characters and numbres: # $ ! @ 5 9"
+
+# name = "Boris"
+# revenue = "$23 dollars"
+# puts name, revenue
+
+# space = " "
+# p space.length
+
+# puts 
+
+# empty = ""
+# p empty.legnth
+
+# puts
+
+# p name.class
+# p space.class
+# p revenue.class
+# p empty.class
+
+# name = String.new("Dan ")
+# p name
+
+p 5.to_s.class
