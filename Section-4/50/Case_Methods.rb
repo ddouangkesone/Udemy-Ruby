e6bfa9ffbed595ac9b4ef9dfec628dff5102f13b
Daniel Douangkesone <karateporkchop@gmail.com>
2018-12-19 14:27:37 -0600

    finished

---
+++ Section-4/50/Case_Methods.rb
@@ -0,0 +1,14 @@
+# puts "hello".capitalize.class
+# puts "heLLo!@".capitalize.class
+# puts "Hello 56".capitalize.class
+# puts "hello world".capitalize.class
+
+# puts "dan123".upcase
+# puts "blah blah blah".upcase
+
+# lowercase = "I'm patient"
+# uppercase = lowercase.upcase
+
+# puts uppercase
+
+p "Dan".downcase
