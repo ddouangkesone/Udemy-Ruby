5215957876a349985233b6dd01c33961d824a237
Daniel Douangkesone <karateporkchop@gmail.com>
2019-01-11 12:40:42 -0600

    finishing up the rest of the code

--- Section-5/64/Multiple_Conditions_with-&&.rb
+++ Section-5/64/Multiple_Conditions_with-&&.rb
@@ -4,4 +4,8 @@ id = true
 
 if age > 21 && ticket && id
   puts "Congradulations, welcome to the show!"
+elsif ticket && id
+  puts "Alright, you get a ticket"
+else
+  puts "You dont have a ticket"
 end
