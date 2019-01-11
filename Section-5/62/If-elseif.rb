1dd9cac207fec1242c14361c1db5845dd9e78b3a
Daniel Douangkesone <karateporkchop@gmail.com>
2019-01-11 12:15:59 -0600

    Lesson 62 Finished

---
+++ Section-5/62/If-elseif.rb
@@ -0,0 +1,23 @@
+# color = "Yellow"
+
+# if color == "red"
+#   puts "Red is rad"
+# elsif color == "Green"
+#   puts "Green is great!"
+# elsif color == "Yellow"
+#   puts "Yay for yellow!"
+# end
+
+# if color == "Green"
+#   puts "Green is great"
+# end
+
+number = 55
+
+if number < 25
+  puts "Thats a low number!"
+elsif number < 50
+  puts "Thats a number in the middle!"
+elsif number < 75
+  puts "Thats a big number!"
+end
